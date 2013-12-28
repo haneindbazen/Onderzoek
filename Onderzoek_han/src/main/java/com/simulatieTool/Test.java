ed7a4477603138ecb48579cefdd7cd34eb55a8b5
ndizigiye <ndiarmand11@yahoo.fr>
2013-12-28 18:17:24 +0100

    new changes

--- Onderzoek_han/src/main/java/com/simulatieTool/Test.java
+++ Onderzoek_han/src/main/java/com/simulatieTool/Test.java
@@ -1,24 +1,64 @@
 package com.simulatieTool;
 
+import java.awt.List;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.HtmlSerializer;
+import org.htmlcleaner.SimpleHtmlSerializer;
+import org.htmlcleaner.TagNode;
 import org.htmlcleaner.HtmlCleaner.*;
 
 public class Test {
 
-	public static void main(String[] args) throws Exception{
-		
-		File prototypesDir = new File("C:/Users/ndizigiye/Pictures/simulator/html/Prototype1");
-		File homePrototypesDir = new File(Test.class.getResource("/interfaces/"+prototypesDir.getName()).getPath());
+	public static File homePrototypesDir;
+	public static org.htmlcleaner.HtmlCleaner cleaner = new org.htmlcleaner.HtmlCleaner();
+	static final CleanerProperties props = new CleanerProperties();
+	static final SimpleHtmlSerializer htmlSerializer =  new SimpleHtmlSerializer(props);
 
-		if(!homePrototypesDir.exists()){
+	public static void main(String[] args) throws IOException {
+
+		File prototypesDir = new File("C:/Users/ndizigiye/Downloads/Prototype1");
+		System.out.println(Test.class.getResource("/interfaces").getPath());
+		System.out.println(prototypesDir.getName());
+		try {
+			homePrototypesDir = new File(Test.class.getResource("/interfaces").getPath() +"/"+prototypesDir.getName());
+		}
+
+		catch (NullPointerException e) {
 			homePrototypesDir.mkdir();
 		}
+
+//		try {
+//			FileUtils.copyDirectory(prototypesDir, homePrototypesDir);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		System.out.println("copyed to: " + homePrototypesDir.getPath());
 		
-		FileUtils.copyDirectory(prototypesDir, homePrototypesDir);
-		//org.htmlcleaner.HtmlCleaner html = new org.htmlcleaner.HtmlCleaner();
-		//html.clean(file);
-	
+		File screenDir = new File(homePrototypesDir.getPath()+"/review/screens");
+		Collection<File> screens = FileUtils.listFiles(screenDir, TrueFileFilter.TRUE, TrueFileFilter.TRUE);
+		
+		ArrayList<File> screenList = new ArrayList<File>(screens);
+		
+		for (File screen : screenList){
+			TagNode node = cleaner.clean(screen);
+			String html = cleaner.getInnerHtml(node);
+			System.out.println("----------------");
+			TagNode screenNode =  node.findElementByAttValue("class" , "screen firer ie-background commentable", true, true);
+			String screenName = screenNode.getAttributeByName("name");
+			File newDest = new File(homePrototypesDir.getPath()+"/review/screens/"+screenName+".html");
+			FileUtils.write(newDest, html, Charset.defaultCharset());
+			FileUtils.forceDelete(screen);
+		}
+		
 	}
 }
