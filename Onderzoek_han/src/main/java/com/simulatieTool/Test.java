365407514c67c83a96665038f0cf204a54bf27dd
ndizigiye <ndiarmand11@yahoo.fr>
2013-12-28 18:54:09 +0100

    new changes

--- Onderzoek_han/src/main/java/com/simulatieTool/Test.java
+++ Onderzoek_han/src/main/java/com/simulatieTool/Test.java
@@ -1,6 +1,4 @@
 package com.simulatieTool;
-
-import java.awt.List;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -8,20 +6,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.htmlcleaner.CleanerProperties;
-import org.htmlcleaner.HtmlSerializer;
 import org.htmlcleaner.SimpleHtmlSerializer;
 import org.htmlcleaner.TagNode;
-import org.htmlcleaner.HtmlCleaner.*;
 
 public class Test {
 
 	public static File homePrototypesDir;
 	public static org.htmlcleaner.HtmlCleaner cleaner = new org.htmlcleaner.HtmlCleaner();
 	static final CleanerProperties props = new CleanerProperties();
-	static final SimpleHtmlSerializer htmlSerializer =  new SimpleHtmlSerializer(props);
+	static final SimpleHtmlSerializer htmlSerializer = new SimpleHtmlSerializer(
+			props);
 
 	public static void main(String[] args) throws IOException {
 
@@ -29,36 +25,45 @@ public class Test {
 		System.out.println(Test.class.getResource("/interfaces").getPath());
 		System.out.println(prototypesDir.getName());
 		try {
-			homePrototypesDir = new File(Test.class.getResource("/interfaces").getPath() +"/"+prototypesDir.getName());
+			homePrototypesDir = new File(Test.class.getResource("/interfaces")
+					.getPath() + "/" + prototypesDir.getName());
+			FileUtils.cleanDirectory(homePrototypesDir);
 		}
 
 		catch (NullPointerException e) {
 			homePrototypesDir.mkdir();
 		}
 
-//		try {
-//			FileUtils.copyDirectory(prototypesDir, homePrototypesDir);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		System.out.println("copyed to: " + homePrototypesDir.getPath());
-		
-		File screenDir = new File(homePrototypesDir.getPath()+"/review/screens");
-		Collection<File> screens = FileUtils.listFiles(screenDir, TrueFileFilter.TRUE, TrueFileFilter.TRUE);
-		
+		 try {
+		 FileUtils.copyDirectory(prototypesDir, homePrototypesDir);
+		 } catch (IOException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		 System.out.println("copyed to: " + homePrototypesDir.getPath());
+
+		File screenDir = new File(homePrototypesDir.getPath()
+				+ "/review/screens");
+		Collection<File> screens = FileUtils.listFiles(screenDir,
+				TrueFileFilter.TRUE, TrueFileFilter.TRUE);
+
 		ArrayList<File> screenList = new ArrayList<File>(screens);
-		
-		for (File screen : screenList){
+
+		for (File screen : screenList) {
 			TagNode node = cleaner.clean(screen);
 			String html = cleaner.getInnerHtml(node);
 			System.out.println("----------------");
-			TagNode screenNode =  node.findElementByAttValue("class" , "screen firer ie-background commentable", true, true);
+			TagNode screenNode = node.findElementByAttValue("class",
+					"screen firer ie-background commentable", true, true);
 			String screenName = screenNode.getAttributeByName("name");
-			File newDest = new File(homePrototypesDir.getPath()+"/review/screens/"+screenName+".html");
-			FileUtils.write(newDest, html, Charset.defaultCharset());
-			FileUtils.forceDelete(screen);
+			File newDest = new File(homePrototypesDir.getPath()
+					+ "/review/screens/" + screenName + ".html");
+
+			if (!newDest.exists()) {
+				FileUtils.write(newDest, html, Charset.defaultCharset());
+				FileUtils.forceDelete(screen);
+			}
 		}
-		
+
 	}
 }
